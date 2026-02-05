52ec477b4f39dcaa3e9803f632283aa0d283d43b
Cortex <team@getcortexapp.com>
2026-02-05 06:54:39 +0000

    Initial Commit

---
+++ src/test/java/com/example/maven_test_docker/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven_test_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
